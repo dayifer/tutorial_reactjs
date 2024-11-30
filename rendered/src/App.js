47c0096e39c027993b23ebe63954060857828675
dayifer <dayanac.ovalle@gmail.com>
2024-11-30 12:35:15 -0400

    Mapeo y Renderizado

---
+++ rendered/src/App.js
@@ -0,0 +1,22 @@
+const products = [
+  { title: 'Col', isFruit: false, id: 1 },
+  { title: 'Ajo', isFruit: false, id: 2 },
+  { title: 'Manzana', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+      <li
+          key={product.id}
+          style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+          }}
+      >
+        {product.title}
+      </li>
+  );
+
+  return (
+      <ul>{listItems}</ul>
+  );
+}
